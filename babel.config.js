0d4717383ef77979e40d0228f4668403697e7190
Neidazeballostejada <zeballosneida@gmail.com>
2025-09-23 15:58:10 -0400

    Cambios

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,8 @@ module.exports = function (api) {
           path: ".env",
         },
       ],
-      "nativewind/babel"
+      "nativewind/babel",
+      "react-native-worklets/plugin" 
     ],
   };
 };
